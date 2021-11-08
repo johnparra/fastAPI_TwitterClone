e54ce9ebd6e160a05d03ec9c8c3d1a8593981fd0
John Parra <ingsis.johnparra@gmail.com>
2021-11-07 21:02:37 -0500

    skull_users

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#python
+from typing import List
+
+#python and pydantic
 from models import User
 
 from fastapi import FastAPI
+from fastapi import status
 
 
 app = FastAPI()
@@ -22,3 +27,63 @@ def home():
     return {
         "tweets": "¡Working!"
     }
+
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register User",
+    tags=["users"]
+)
+def signup():
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login User",
+    tags=["users"]
+)
+def login():
+    pass
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show List Users",
+    tags=["users"]
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show One User",
+    tags=["users"]
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["users"]
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["users"]
+)
+def update_a_user():
+    pass
